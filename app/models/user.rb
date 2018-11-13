64d233051340eee6ba8f7136cf5170d7adfa349c
Justin <misterjustincox@gmail.com>
2018-11-13 11:51:34 -0500

    Create Sessions controller

---
+++ app/models/user.rb
@@ -0,0 +1,48 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+
+  validates :username, :session_token, :password_digest, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after_initialize :ensure_session_token!
+  attr_reader :password
+
+  def self.find_by_credentials(passed_username, passed_word)
+    user = User.find_by(username: passed_username)
+    return nil unless user && user.is_password?(passed_word)
+    user
+  end
+
+  def password=(new_password)
+    @password = new_password
+    self.password_digest = BCrypt::Password.create(new_password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def is_password?(passed_word)
+    BCrypt::Password.new(self.password).is_password?(passed_word)
+  end
+
+  def ensure_session_token!
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+end
