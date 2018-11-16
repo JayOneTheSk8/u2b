9b164bb6d5f9472fb8b79a8837b21bb8cad7eb97
Justin <misterjustincox@gmail.com>
2018-11-16 15:13:07 -0500

    Create working Active Storage space

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,12 +1,20 @@
 class Api::VideosController < ApplicationController
-  def index  
+  def index
   end
 
   def create
+    @video = Video.new(video_params)
+    @video.uploader_id = current_user.id
+    if @video.save
 
+    else
+      render json: @video.errors.full_messages, status: 422
+    end
   end
 
   def show
+    @video = Video.find(params[:id])
+    render :show
   end
 
   def update
@@ -16,4 +24,10 @@ class Api::VideosController < ApplicationController
   def destroy
 
   end
+
+  # private
+  #
+  # def video_params
+  #   params.require(:video).permit(:title, :description, :video)
+  # end
 end
