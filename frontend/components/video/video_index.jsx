5b3d5c973cbddd08386d7e830dcb5dde34c27a2e
Justin <misterjustincox@gmail.com>
2018-11-19 17:56:34 -0500

    Render all videos on index

--- frontend/components/video/video_index.jsx
+++ frontend/components/video/video_index.jsx
@@ -3,11 +3,23 @@ import { Link } from 'react-router-dom';
 
 class VideoIndex extends React.Component {
 
+  componentDidMount() {
+    this.props.fetchVideos();
+  }
+
   render() {
+    const videos = this.props.videos.map((video) => {
+      // debugger
+      return (
+        <li key={video.id}>{JSON.stringify(video)}</li>
+      );
+    });
     return (
       <>
         <h1 className="greeting">Welcome to U2B! {this.props.currentUser.username}</h1>
-        
+        <ul>
+          {videos}
+        </ul>
       </>
     );
   }
--- frontend/components/video/video_index_container.js
+++ frontend/components/video/video_index_container.js
@@ -1,17 +1,20 @@
 import { connect } from 'react-redux';
 import VideoIndex from './video_index';
 import { logout } from '../../actions/session_actions';
+import { fetchVideos } from '../../actions/video_actions';
 
 const mapStateToProps = state => {
   const currentUser = state.entities.users[state.session.currentUserId] || { username: "" }
   return {
     currentUser,
+    videos: Object.keys(state.entities.videos).map((id) => state.entities.videos[id]),
     loggedIn: Boolean(state.session.currentUserId)
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
+    fetchVideos: () => dispatch(fetchVideos()),
     logout: () => dispatch(logout())
   };
 };
