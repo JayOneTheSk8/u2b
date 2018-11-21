6fba45c5d97182fafc7a838b7e86be12e3cfff31
Justin <misterjustincox@gmail.com>
2018-11-21 16:34:37 -0500

    Start action creators

---
+++ frontend/actions/ui_actions.js
@@ -0,0 +1,12 @@
+export const OPEN_DROPDOWN_MENU = 'OPEN_DRAWER';
+export const OPEN_USER_DRAWER = 'OPEN_USER_DRAWER';
+
+const openDropdownMenu = () => {
+  return {
+    type: OPEN_DROPDOWN_MENU
+  };
+};
+
+export const openDropdownMenu = () => dispatch => {
+  return dispatch({ type:  });
+};
