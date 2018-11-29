0ed8b98c94fff685d78489c3c6e198ba03e8d8c4
Justin <misterjustincox@gmail.com>
2018-11-29 14:00:37 -0500

    Delete comments ratings with videos

--- frontend/u2b.jsx
+++ frontend/u2b.jsx
@@ -5,6 +5,8 @@ import configureStore from './store/store';
 import Root from './components/root';
 import * as VideoAPIUtil from './util/video_api_util';
 
+const obj = { x: 1 };
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   let store;
