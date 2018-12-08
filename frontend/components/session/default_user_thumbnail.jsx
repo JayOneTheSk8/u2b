d78a285c17d521e9b715d5f87a3dee91e752b3d1
Justin <misterjustincox@gmail.com>
2018-12-08 13:24:37 -0500

    Setup css for thumbnail change

--- frontend/components/session/default_user_thumbnail.jsx
+++ frontend/components/session/default_user_thumbnail.jsx
@@ -3,12 +3,18 @@ import React from 'react';
 const DefaultUserThumbnail = props => {
   if (Object.values(props.thumbnailInfo).indexOf(null) > -1) {
     return (
-      <div className="default-user-circle">
-        <p className="default-first-letter">{props.username[0].toUpperCase()}</p>
+      <div className="default-user-border">
+        <div className="default-user-circle">
+          <p className="default-first-letter">{props.username[0].toUpperCase()}</p>
+        </div>
       </div>
     );
   } else {
-    return null;
+    <div className={`user-border-${props.thumbnailInfo.border}`}>
+      <div className={`user-circle-${props.thumbnailInfo.background}`}>
+        <p className={`first-letter-${props.thumbnailInfo.letter}`}>{props.username[0].toUpperCase()}</p>
+      </div>
+    </div>
   }
 };
 
