82de7bf78ad609cab9ef6abff4e0e48d5c4ee9d2
Justin <misterjustincox@gmail.com>
2018-12-08 13:40:44 -0500

    Make update route and controller action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -9,6 +9,15 @@ class Api::UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
   def search
     query = params[:search]
     @videos = Video.where("title iLIKE :query", query: "#{query}%").distinct.limit(7)
