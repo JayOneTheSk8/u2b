d20aae94c15ebeb749b3bc1a9382a8c86db525e9
Justin <misterjustincox@gmail.com>
2018-12-09 13:04:36 -0500

    Create constatntly updating field for uer decal

--- frontend/components/video/user_videos/user_edit.jsx
+++ frontend/components/video/user_videos/user_edit.jsx
@@ -1,10 +1,83 @@
 import React from 'react';
+import { merge } from 'lodash';
 
 class UserEditForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = merge({}, this.props.currentUser);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  update(field) {
+    return (e) => {
+      this.setState({ [field]: e.target.value });
+    };
+  }
+
+  letterDecal() {
+    if (this.state.username) {
+      return (
+        <p className={`first-letter-${this.state.thumbnailLetter}`}>{this.state.username[0].toUpperCase()}</p>
+      );
+    } else {
+      return null;
+    }
+  }
+
   render() {
     return (
-      <div>
-        <p>EDIT PAGE</p>
+      <div className="user-edit-area">
+
+        <div className="thumbnail-preview">
+          <div className={`user-border-${this.state.thumbnailBorder}`}>
+            <div className={`user-circle-${this.state.thumbnailBackground}`}>
+              { this.letterDecal() }
+            </div>
+          </div>
+        </div>
+
+        <form className="user-edit-form" onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.update('username')} value={this.state.username} />
+          <select value={this.state.thumbnailBorder} className="border-selection" onChange={this.update('thumbnailBorder')}>
+            <option disabled={this.props.borderColors.red} value="red">Red</option>
+            <option disabled={this.props.borderColors.orange} value="orange">Orange</option>
+            <option disabled={this.props.borderColors.yellow} value="yellow">Yellow</option>
+            <option disabled={this.props.borderColors.green} value="green">Green</option>
+            <option disabled={this.props.borderColors.blue} value="blue">Blue</option>
+            <option disabled={this.props.borderColors.purple} value="purple">Purple</option>
+            <option disabled={this.props.borderColors.black} value="black">Black</option>
+            <option disabled={this.props.borderColors.pink} value="pink">Pink</option>
+            <option disabled={this.props.borderColors.brown} value="brown">Brown</option>
+          </select>
+          <select value={this.state.thumbnailBackground} className="circle-selection" onChange={this.update('thumbnailBackground')}>
+            <option disabled={this.props.circleColors.red} value="red">Red</option>
+            <option disabled={this.props.circleColors.orange} value="orange">Orange</option>
+            <option disabled={this.props.circleColors.yellow} value="yellow">Yellow</option>
+            <option disabled={this.props.circleColors.green} value="green">Green</option>
+            <option disabled={this.props.circleColors.blue} value="blue">Blue</option>
+            <option disabled={this.props.circleColors.purple} value="purple">Purple</option>
+            <option disabled={this.props.circleColors.black} value="black">Black</option>
+            <option disabled={this.props.circleColors.white} value="white">White</option>
+            <option disabled={this.props.circleColors.pink} value="pink">Pink</option>
+            <option disabled={this.props.circleColors.brown} value="brown">Brown</option>
+          </select>
+          <select value={this.state.thumbnailLetter} className="letter-selection" onChange={this.update('thumbnailLetter')}>
+            <option disabled={this.props.letterColors.red} value="red">Red</option>
+            <option disabled={this.props.letterColors.orange} value="orange">Orange</option>
+            <option disabled={this.props.letterColors.yellow} value="yellow">Yellow</option>
+            <option disabled={this.props.letterColors.green} value="green">Green</option>
+            <option disabled={this.props.letterColors.blue} value="blue">Blue</option>
+            <option disabled={this.props.letterColors.purple} value="purple">Purple</option>
+            <option disabled={this.props.letterColors.black} value="black">Black</option>
+            <option disabled={this.props.letterColors.white} value="white">White</option>
+            <option disabled={this.props.letterColors.pink} value="pink">Pink</option>
+            <option disabled={this.props.letterColors.brown} value="brown">Brown</option>
+          </select>
+        </form>
       </div>
     );
   }
--- frontend/components/video/user_videos/user_edit_container.js
+++ frontend/components/video/user_videos/user_edit_container.js
@@ -8,19 +8,59 @@ const mapStateToProps = state => {
   const letter = state.session.thumbnailInfo.letter || 'white';
   const defaultSettings = {
     id: state.session.currentUserId,
-    thumbnail_border: 'black',
-    thumbnail_background: 'green',
-    thumbnail_letter: 'white',
+    username: state.entities.users[state.session.currentUserId].username,
+    thumbnailBorder: 'black',
+    thumbnailBackground: 'green',
+    thumbnailLetter: 'white',
   };
   const currentUser = {
     id: state.session.currentUserId,
-    thumbnail_border: border,
-    thumbnail_background: background,
-    thumbnail_letter: letter,
+    username: state.entities.users[state.session.currentUserId].username,
+    thumbnailBorder: border,
+    thumbnailBackground: background,
+    thumbnailLetter: letter,
+  };
+  const borderColors = {
+    red: (currentUser.thumbnailBorder === "red" ? true : false),
+    orange: (currentUser.thumbnailBorder === "orange" ? true : false),
+    yellow: (currentUser.thumbnailBorder === "yellow" ? true : false),
+    green: (currentUser.thumbnailBorder === "green" ? true : false),
+    blue: (currentUser.thumbnailBorder === "blue" ? true : false),
+    purple: (currentUser.thumbnailBorder === "purple" ? true : false),
+    black: (currentUser.thumbnailBorder === "black" ? true : false),
+    pink: (currentUser.thumbnailBorder === "pink" ? true : false),
+    brown: (currentUser.thumbnailBorder === "brown" ? true : false)
+  };
+  const circleColors = {
+    red: (currentUser.thumbnailBackground === "red" ? true : false),
+    orange: (currentUser.thumbnailBackground === "orange" ? true : false),
+    yellow: (currentUser.thumbnailBackground === "yellow" ? true : false),
+    green: (currentUser.thumbnailBackground === "green" ? true : false),
+    blue: (currentUser.thumbnailBackground === "blue" ? true : false),
+    purple: (currentUser.thumbnailBackground === "purple" ? true : false),
+    white: (currentUser.thumbnailBackground === "white" ? true : false),
+    black: (currentUser.thumbnailBackground === "black" ? true : false),
+    pink: (currentUser.thumbnailBackground === "pink" ? true : false),
+    brown: (currentUser.thumbnailBackground === "brown" ? true : false)
+  };
+  const letterColors = {
+    red: (currentUser.thumbnailLetter === "red" ? true : false),
+    orange: (currentUser.thumbnailLetter === "orange" ? true : false),
+    yellow: (currentUser.thumbnailLetter === "yellow" ? true : false),
+    green: (currentUser.thumbnailLetter === "green" ? true : false),
+    blue: (currentUser.thumbnailLetter === "blue" ? true : false),
+    purple: (currentUser.thumbnailLetter === "purple" ? true : false),
+    white: (currentUser.thumbnailLetter === "white" ? true : false),
+    black: (currentUser.thumbnailLetter === "black" ? true : false),
+    pink: (currentUser.thumbnailLetter === "pink" ? true : false),
+    brown: (currentUser.thumbnailLetter === "brown" ? true : false)
   };
   return {
     currentUser,
     defaultSettings,
+    borderColors,
+    circleColors,
+    letterColors,
   };
 };
 
