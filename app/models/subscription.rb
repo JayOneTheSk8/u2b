1c96d1211f0da78e6f498d0d490d396b05cc032d
Justin <misterjustincox@gmail.com>
2018-12-10 11:49:38 -0500

    Add custom validation where user cannot subscribe to thei own channel

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,6 @@
 class Subscription < ApplicationRecord
   validates :user_id, uniqueness: { scope: :channel_id }
-  # validate :not_own_channel
+  validate :not_own_channel
 
   belongs_to :subscriber,
     primary_key: :id,
@@ -11,8 +11,10 @@ class Subscription < ApplicationRecord
     primary_key: :id,
     foreign_key: :channel_id,
     class_name: :User
-  
-  # def not_own_channel
-  #
-  # end
+
+  def not_own_channel
+    if self.user_id == self.channel_id
+      errors.add(:user_id, 'cannot subscribe to own channel')
+    end
+  end
 end
