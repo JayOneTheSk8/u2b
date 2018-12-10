fa5ecbe3cb4c527ef233a3489dc031855aa6608f
Justin <misterjustincox@gmail.com>
2018-12-10 15:32:57 -0500

    Render subscribed videos on  subscriptions page

--- frontend/components/video/subscriptions/subscriptions_page.jsx
+++ frontend/components/video/subscriptions/subscriptions_page.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import MinimisedVideo from '../show/minimised_video';
 
 class SubscriptionsPage extends React.Component {
+  componentDidMount() {
+    this.props.fetchSubscriptions(this.props.currentUserId);
+  }
+
   render(){
+    const videos = this.props.videos.map((video) => {
+      return (
+        <MinimisedVideo key={video.id} video={video}/>
+      );
+    });
     return (
       <>
         <p>Subscriptions Page</p>
+        <ul>
+          {videos}
+        </ul>
       </>
     );
   }
--- frontend/components/video/subscriptions/subscriptions_page_container.js
+++ frontend/components/video/subscriptions/subscriptions_page_container.js
@@ -1,4 +1,31 @@
 import { connect } from 'react-redux';
 import SubscriptionsPage from './subscriptions_page';
+import { fetchSubscriptions } from '../../../actions/video_actions';
 
-export default connect(null, null)(SubscriptionsPage);
+const mapStateToProps = state => {
+  const loggedIn = Boolean(state.session.currentUserId);
+  const currentUserId = state.session.currentUserId;
+  let videos = state.entities.videos.subscriptions || [];
+  videos = Object.keys(videos).map(
+    id => state.entities.videos.subscriptions[id]
+  );
+  for (let i = 0; i < videos.length; i++) {
+    videos[i].uploaderName = state.entities.users[videos[i].uploader_id].username;
+  }
+  return {
+    loggedIn,
+    currentUserId,
+    videos,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchSubscriptions: userId => dispatch(fetchSubscriptions(userId)),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SubscriptionsPage);
